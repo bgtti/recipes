38e882807f6d3907912783486ce6ed97f8783339
bgtti <bru.mgtti@gmail.com>
2022-09-25 22:48:38 +0200

    home page

--- src/01-HomePage.js
+++ src/01-HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-// import './01-HomePage.css';
-// import XX from './Images/XX.png' //Source: ...
+import './01-HomePage.css';
+import Footer from './03-Footer';
+import IconMain from './Images/IconMain.png'
 
 class HomePage extends Component {
     // constructor(props) {
@@ -9,7 +10,22 @@ class HomePage extends Component {
     render() {
         return (
             <div className='HomePage'>
-                <h1>Favorite Recipes</h1>
+                <div className='HomePage-Header'>
+                    <img src={IconMain} alt="Logo" />
+                    <h1><span className='HomePage-H1-Part1' >Favorite</span> <span className='HomePage-H1-Part2'>Recipes</span></h1>
+                </div>
+
+                <div className='HomePage-Content'>
+                    <div className='HomePage-NavContainer'>
+                        <h2>What would you like to cook?</h2>
+                        <nav>
+                            <p className='HomePage-NavLink'>Salad</p>
+                            <p className='HomePage-NavLink'>Meal</p>
+                            <p className='HomePage-NavLink'>Dessert</p>
+                        </nav>
+                    </div>
+                </div>
+                <Footer></Footer>
             </div>
         )
     }
