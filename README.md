a6e592761a1f5b3671a2ff9175d3a4d4bc4adc45
bgtti <bru.mgtti@gmail.com>
2022-09-25 19:38:37 +0200

    skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Favorite Recipes
+
+Live site: [Click here](https://...).
---
+++ src/01-HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import './01-HomePage.css';
+// import XX from './Images/XX.png' //Source: ...
+
+class HomePage extends Component {
+    // constructor(props) {
+    //     super(props);
+    // };
+    render() {
+        return (
+            <div className='HomePage'>
+                <h1>Favorite Recipes</h1>
+            </div>
+        )
+    }
+}
+export default HomePage;
